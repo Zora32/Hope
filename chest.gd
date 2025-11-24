295eee6f0b332b9386c69c526488586401f223fb
Zora32 <zoracajkovskij779@gmail.com>
2025-11-24 17:02:29 +0500

    Chest

--- chest.gd
+++ chest.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
-@export var items: Array[InventoryItem]
+
+@export var item: Array[InventoryItem]
+var coint_preload = preload("res://Collectobls/lifepot.tscn")
 @onready var animations: AnimationPlayer = $AnimationPlayer
 @onready var item_start_pos: Vector2 = $ItemStartPos.position
 @onready var item_end_pos: Vector2 = $itemEndPos.position
@@ -10,8 +12,7 @@ var is_open: bool = false
 
 func interact(interacter: Node2D):
 	if is_open: return
-	var inventory = interacter.get("inventory")
-	if !inventory or inventory is not Inventory: return
+
 
 	is_open = true
 	animations.play("open")
@@ -19,4 +20,21 @@ func interact(interacter: Node2D):
 	spawn_and_collect(interacter)
 
 func spawn_and_collect(interacter: Node2D):
-	pass 
+	
+	for i: InventoryItem in item:
+		for a in range(1):
+			var sprite := Sprite2D.new()
+			sprite.texture = i.texture
+			sprite.position = item_start_pos
+			add_child(sprite)
+			
+			var tween = create_tween()
+			tween.tween_property(sprite, "position", item_end_pos, 0.3)
+			await tween.finished
+			
+			var tween_collect = create_tween()
+			tween_collect.tween_property(sprite, "global_position", interacter.global_position, 0.4)
+			tween_collect.tween_callback(func():
+				sprite.queue_free()
+				interacter.inventory.insert(i))
+   
